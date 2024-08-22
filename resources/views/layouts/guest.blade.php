398f41603d0c1561166cdda797063d4743d3dffc
JonatasTB <101901086+jonatastb@users.noreply.github.com>
2024-08-22 15:22:23 -0300

    :lipstick: feat: List, Edit, New Products screen created, responsive content included

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,14 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-row items-center pt-6 sm:pt-0 bg-gray-100">
-            <div class="w-full bg-indigo-500 h-screen flex justify-center items-center shadow-2xl shadow-">
-                <h1 class="text-7xl text-center font-extrabold text-white">
+        <div class="lg:min-h-screen h-auto flex flex-col lg:flex-row items-center sm:pt-0 bg-gray-100">
+            <div class="w-full bg-indigo-500 lg:h-screen flex justify-center items-center shadow-2xl shadow-">
+                <h1 class="lg:text-7xl text-2xl text-center font-extrabold p-5 lg:p-0 text-white">
                     Gerenciador <br> de <br> Produtos
                 </h1>
             </div>
-            <div class="w-1/2 px-8 flex items-center justify-center">
-                <div class="w-1/2 mt-6 px-6 py-4 bg-white shadow-md sm:rounded-sm">
+            <div class="lg:w-1/2 w-full lg:h-auto h-[82vh] px-8 flex items-center justify-center">
+                <div class="lg:w-1/2 w-full mt-6 px-6 py-4 bg-white shadow-md sm:rounded-sm">
                     {{ $slot }}
                 </div>
             </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,12 +5,12 @@
             <div class="flex">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('product.index')" :active="request()->routeIs('product.index')">
                         {{ __('Meus produtos') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('new-product')" :active="request()->routeIs('new-product')">
+                    <x-nav-link :href="route('product.create')" :active="request()->routeIs('product.create')">
                         {{ __('Novo produto') }}
                     </x-nav-link>
                 </div>
@@ -65,8 +65,13 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('product.index')" :active="request()->routeIs('product.index')">
+                {{ __('Meus Produtos') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('product.create')" :active="request()->routeIs('product.create')">
+                {{ __('Novo produto') }}
             </x-responsive-nav-link>
         </div>
 
